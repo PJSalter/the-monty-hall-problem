dda5b531304a29cfc4e3e5d0f9a2c631367ab299
PJSalter <psalter88@googlemail.com>
2022-03-24 02:47:39 +0000

    console logs for testing each DOM attributes variable name so that they can be seen in browser console

--- app.js
+++ app.js
@@ -10,27 +10,42 @@ console.log(wholeBody);
 const instruct = document.querySelector('#to-do');
 console.log(instruct);
 
-const FirstRow = document.querySelector('#rowNumOne');
-
+const firstRow = document.querySelector('#rowNumOne');
+console.log(firstRow);
 const secondRow = document.querySelector('#rowNumTwo');
+console.log(secondRow);
 
 //DOM manipulation for all three propability doors.
 const d1 = document.querySelector('#doorNumOne');
+console.log(d1);
 const d2 = document.querySelector('#doorNumTwo');
+console.log(d2);
 const d3 = document.querySelector('#doorNumThree');
+console.log(d3);
 
 // buttons for yes or no.
 const switchForYes = document.getElementById('yes-button');
+console.log(switchForYes);
 const switchForNo = document.getElementById('no-button');
+console.log(switchForNo);
 
 // picking up the images for each door.
 const doorImgOne = document.getElementById('doorOne');
+console.log(doorImgOne);
 const doorImgTwo = document.getElementById('doorTwo');
+console.log(doorImgTwo);
 const doorImgThree = document.getElementById('doorThree');
+console.log(doorImgThree);
 
 // DOM picking up the probabilities.
 const SwitchAndWin = document.querySelector("#choiceIsYours");
+console.log(SwitchAndWin);
 const SwitchAndLose = document.querySelector("#LossPropability");
+console.log(SwitchAndLose);
 const NoSwitchAndWin = document.querySelector("#chooseToNotSwitchAndWin");
+console.log(NoSwitchAndWin);
 const NoSwitchAndLose = document.querySelector("#NoSwitchAndLose");
+console.log(NoSwitchAndLose);
+
+
 
